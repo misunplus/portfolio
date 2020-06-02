196dcf12602bd5f67bd0ff01aa60a788952f2d0b
Beckia <63723700+misunplus@users.noreply.github.com>
2020-06-02 17:52:36 +0900

    JS:navbar,contact

--- main.js
+++ main.js
@@ -6,10 +6,45 @@ const navbarHeight = navbar.getBoundingClientRect().height;
 
 // Make bavbar transparent when it is on the top
 // 스크롤이 될때마다 ()=>{} 실행
-document.addEventListener('scroll', ()=>{
-    if (window.scrollY > navbarHeight){
+// document.addEventListener('scroll', ()=>{
+//     if (window.scrollY > navbarHeight){
+//         navbar.classList.add('navbar--dark');
+//     }else{
+//         navbar.classList.remove('navbar--dark');
+//     }
+// } )
+
+document.addEventListener('scroll',()=>{
+    if(window.scrollY>navbarHeight){
         navbar.classList.add('navbar--dark');
     }else{
         navbar.classList.remove('navbar--dark');
     }
-} )
+});
+
+
+// handle navr item move
+const navberMenu = document.querySelector('.navbar__menu');
+
+navberMenu.addEventListener('click', ()=>{
+const targetLink = event.target.dataset.link;
+    if(targetLink == null){
+        return;        
+    }else{
+        const scrollTo = document.querySelector(targetLink);
+        scrollTo.scrollIntoView({behavior: 'smooth'});
+    }
+});
+
+// Handle Contack me Button move
+const conTackBt =  document.querySelector('.home__contact');
+
+conTackBt.addEventListener('click',()=>{
+const targetLink = event.target.dataset.link;
+    if(targetLink==null){
+        return;
+    }else{
+        const scrollTo = document.querySelector(targetLink);
+        scrollTo.scrollIntoView({behavior:'smooth'});
+    }
+})
