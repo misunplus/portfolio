bfb548687535549fc9ea2d7300ba1a4477bbdbb3
Beckia <misunplus@gmail.com>
2020-08-13 10:11:41 +0900

    수정

--- main.js
+++ main.js
@@ -4,113 +4,106 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
-document.addEventListener('scroll',()=>{
-    if(window.scrollY>navbarHeight){
-        navbar.classList.add('navbar--dark');
-    }else{
-        navbar.classList.remove('navbar--dark');
-    }
+document.addEventListener('scroll', () => {
+	if (window.scrollY > navbarHeight) {
+		navbar.classList.add('navbar--dark');
+	} else {
+		navbar.classList.remove('navbar--dark');
+	}
 });
-
 
 // handle navr item move
 const navberMenu = document.querySelector('.navbar__menu');
 
-navberMenu.addEventListener('click', ()=>{
-    const targetLink = event.target.dataset.link;
-    if(targetLink == null){
-        return;        
-    }else{
-        navberMenu.classList.add('close');
-        scrollIntoview(targetLink);
-    }
+navberMenu.addEventListener('click', () => {
+	const targetLink = event.target.dataset.link;
+	if (targetLink == null) {
+		return;
+	} else {
+		navberMenu.classList.add('close');
+		scrollIntoview(targetLink);
+	}
 });
 
 //Navbar toggle menu
 const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
-navbarToggleBtn.addEventListener('click', ()=>{
-    navberMenu.classList.toggle('close');
-})
-
-
-// Handle Contack me Button move
-const conTackBt =  document.querySelector('.home__contact');
-
-conTackBt.addEventListener('click',()=>{
-const targetLink = event.target.dataset.link;
-    if(targetLink==null){
-        return;
-    }else{
-        const scrollTo = document.querySelector(targetLink);
-        scrollIntoview(targetLink);
-    }
+navbarToggleBtn.addEventListener('click', () => {
+	navberMenu.classList.toggle('close');
 });
 
+// Handle Contack me Button move
+const conTackBt = document.querySelector('.home__contact');
+
+conTackBt.addEventListener('click', () => {
+	const targetLink = event.target.dataset.link;
+	if (targetLink == null) {
+		return;
+	} else {
+		const scrollTo = document.querySelector(targetLink);
+		scrollIntoview(targetLink);
+	}
+});
 
 //home scrooll transparent
 const home = document.querySelector('#home');
 const home__ct = document.querySelector('.home__ct');
 const homeHeight = home.getBoundingClientRect().height;
 
-document.addEventListener('scroll', ()=>{
-   home__ct.style.opacity = 1- window.scrollY/homeHeight;
+document.addEventListener('scroll', () => {
+	home__ct.style.opacity = 1 - window.scrollY / homeHeight;
 });
-
 
 //Arrow up handling
 const arrowUp = document.querySelector('.arrow__up');
-document.addEventListener('scroll', () =>{
-    if(window.scrollY>homeHeight/2){
-        arrowUp.classList.add('visible');
-    }else{
-        arrowUp.classList.remove('visible');
-    }
-})
-
-arrowUp.addEventListener('click', ()=>{
-    const homeLink = ("#home");
-    scrollIntoview(homeLink);
+document.addEventListener('scroll', () => {
+	if (window.scrollY > homeHeight / 2) {
+		arrowUp.classList.add('visible');
+	} else {
+		arrowUp.classList.remove('visible');
+	}
 });
 
+arrowUp.addEventListener('click', () => {
+	const homeLink = '#home';
+	scrollIntoview(homeLink);
+});
 
 //Work filtering
 const work__projects = document.querySelector('.work__projects');
 const project = document.querySelectorAll('.project');
-document.addEventListener('click', (event)=>{
-    const filter = event.target.dataset.filter || event.target.parentNode.dataset.filter;
-    if (filter == null){
-        return;
-    }
+document.addEventListener('click', event => {
+	const filter = event.target.dataset.filter || event.target.parentNode.dataset.filter;
+	if (filter == null) {
+		return;
+	}
 
-    //Remove Selection Active
-    const active = document.querySelector('.category__btn.selected');
-    if(active !=null){
-        active.classList.remove('selected');
-    }
-    event.target.classList.add('selected');
-    
-    // let projects;
-    // for(let i=0; i<project.length; ++i){
-    //     projects = project[i];
-    // } 
-    work__projects.classList.add('ani-out');
+	//Remove Selection Active
+	const active = document.querySelector('.category__btn.selected');
+	if (active != null) {
+		active.classList.remove('selected');
+	}
+	event.target.classList.add('selected');
 
-    setTimeout(()=>{
-    work__projects.classList.remove('ani-out');   
-    }, 300);
-    project.forEach((project)=>{
-        if(filter === project.dataset.type || filter === "*"){
-            project.classList.remove('invisible');
-        } else{
-            project.classList.add('invisible'); 
-        }  
-    });
+	// let projects;
+	// for(let i=0; i<project.length; ++i){
+	//     projects = project[i];
+	// }
+	work__projects.classList.add('ani-out');
+
+	setTimeout(() => {
+		work__projects.classList.remove('ani-out');
+	}, 300);
+	project.forEach(project => {
+		if (filter === project.dataset.type || filter === '*') {
+			project.classList.remove('invisible');
+		} else {
+			project.classList.add('invisible');
+		}
+	});
 });
 
-
-
 //scrollIntoView smooth
-function scrollIntoview(selecter){
-    const scrollTo = document.querySelector(selecter);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
-};
+function scrollIntoview(selecter) {
+	const scrollTo = document.querySelector(selecter);
+	scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
